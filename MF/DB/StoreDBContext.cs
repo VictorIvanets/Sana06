a7b2c0bc89752e7949b69b5f67718f1a4aa60918
Виктор <imperia.zt@gmail.com>
2025-01-30 14:03:54 +0200

    init auth

--- MF/DB/StoreDBContext.cs
+++ MF/DB/StoreDBContext.cs
@@ -9,10 +9,8 @@ namespace MF.DB
         public StoreDBContext(DbContextOptions<StoreDBContext> options) : base(options) {         }
 
         public DbSet<Store> Store { get; set; }
-        public DbSet<Roles> Roles { get; set; }
         public DbSet<TypesFish> TypesFish { get; set; }
         public DbSet<Fishes> Fishes { get; set; }
-        public DbSet<User> User { get; set; }
         public DbSet<Coords> Coords { get; set; }
         public DbSet<PlaceDb> PlaceDb { get; set; }
         public DbSet<PlaceFishCollection> PlaceFishCollection { get; set; }
